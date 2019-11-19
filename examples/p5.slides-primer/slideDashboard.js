863a75a7ebd2ac5761be78f3f10706e3c8ba6c90
Garrett <gflynn@usc.edu>
2019-11-18 19:12:36 -0800

    More p5 styling

--- examples/p5.slides-primer/slideDashboard.js
+++ examples/p5.slides-primer/slideDashboard.js
@@ -2,13 +2,18 @@
 
 let sketches = {};
 let myDecks = {};
+// img;
 
 function preload() {
  sketches = loadJSON('defaultSketches.json');
  myDecks = loadJSON('myDecks.json');
+    // img = loadImage('icons/object-selected.png');
 }
 
 function setup() {
+     // img.filter(INVERT);
+     // image(img,width/2,0);
+     // img.save('object-selected','png');
   UI = new p5.slidesUI([],sketches);
 }
 
